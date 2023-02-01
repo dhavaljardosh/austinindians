4ea6d6322539d45fcdd1c59037afca29a20adac2
Dhaval Jardosh <dhavaljardosh@gmail.com>
2023-01-31 23:29:55 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/austinindian/precache-manifest.20ebb7626af28958d11d220a5c8dbf15.js"
+  "/austinindians/precache-manifest.6ea2aa06f424bbb0b5472272b0e532a6.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/austinindian/index.html", {
+workbox.routing.registerNavigationRoute("/austinindians/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });

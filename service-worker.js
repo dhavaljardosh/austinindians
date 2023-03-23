c9e6632f07a179f41f93a8b4992983b5563e8158
Dhaval Jardosh <dhavaljardosh@gmail.com>
2023-03-23 01:06:11 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.c886feb9a2d4d3b47cabf257a256d10f.js"
+  "./precache-manifest.354a1fb115e27a553c85bea62a81b9a8.js"
 );
 
 workbox.clientsClaim();
